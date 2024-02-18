0d93d50b5d106d88254bd1695d2860625cfd2895
hans3I <hanselstevb6@gmail.com>
2024-02-18 20:39:08 +0700

    updates

--- .config/spectrwm/.config/spectrwm2/.config/spectrwm/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-setxkbmap -option caps:swapescape\
-          -option compose:lctrl-altgr
-nitrogen --restore
-xrdb ~/.Xresources
-$HOME/.scripts/natscroll
-xss-lock --transfer-sleep-lock -- "betterlockscreen -l dimblur" --nofork &
-mpd &
